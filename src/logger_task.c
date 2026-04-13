32815b62fad6864bbe229db3e3acbea4268eabc9
Utpal Shanker <utpals_21@yahoo.com>
2026-04-13 06:07:32 +0530

    TEMP: commit for code backup

--- src/logger_task.c
+++ src/logger_task.c
@@ -8,15 +8,51 @@
 #include "../inc/extern.h"
 
 
-typedef struct Node{
-    SensorRead_t sPkt;
-    struct Node *next;
-}Node;
+/* append to tail, if count > 100 drop from head */
+typedef struct LogNode{
+    SensorRead_t data;
+    struct LogNode *next;
+}LogNode;
 
-Node *head = NULL;
+LogNode *head = NULL;
 
+void appendNode(void *data)
+{
+    static uint8_t nodeCount = 0;
+    LogNode *currentNode;
+    LogNode *temp = (LogNode*) pvPortMalloc(sizeof(LogNode));
+    temp->data = *(SensorRead_t*)data;
+    temp->next = NULL;
 
+    if (head == NULL) {
+        nodeCount++;
+        head = temp;
+        return;
+    }
+    currentNode = head;
 
+    if(nodeCount == 100)
+    {
+        temp->next = currentNode->next;
+        head = temp;
+        nodeCount--;
+        printf("*** Replaced head node ***\r\n");
+        fflush(stdout);
+        
+    }
+    else if(nodeCount < 100)
+    {
+        while(currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            // printf("Node->data: %.5f\n", currentNode->data.sensorVal);
+            // fflush(stdout);
+        }
+        currentNode->next = temp;
+        nodeCount++;
+    }
+    
+}
 
 void vLoggerTask(void *pvParameters)
 {
@@ -25,10 +61,11 @@ void vLoggerTask(void *pvParameters)
         SensorRead_t receivedData;
         if(xQueueReceive(xMyQueue, &receivedData, portMAX_DELAY) == pdPASS)
         {
+            appendNode(&receivedData);
             // Process the received data (e.g., log it to a file or print it)
-            printf("** TimeStamp: %lu\tADC Val: %.5f **\n", 
-                    receivedData.timestamp, receivedData.sensorVal);
-            fflush(stdout);
+            // printf("** TimeStamp: %lu\tADC Val: %.5f **\n", 
+            //         receivedData.timestamp, receivedData.sensorVal);
+            // fflush(stdout);
         }
     }
 }
